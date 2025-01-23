569254689856ba60309fe15368c8fa8dd04d4bc0
Kergrit Robkop <kergrit@gmail.com>
2025-01-23 12:39:12 +0700

    Added registry stack deployment

---
+++ registry/04-setup-private-registry.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# variables
+BASIC_AUTH_USER=admin
+BASIC_AUTH_PASSWORD=admin
+
+# create auth, data folder
+mkdir -p /home/ubuntu/efs/devops-swarm-cluster-and-observability/registry/data
+mkdir -p /home/ubuntu/efs/devops-swarm-cluster-and-observability/registry/auth
+
+# create basic auth user
+docker run --rm --entrypoint htpasswd httpd:2 -Bbn $BASIC_AUTH_USER $BASIC_AUTH_PASSWORD > /home/ubuntu/efs/devops-swarm-cluster-and-observability/registry/authhtpasswd
