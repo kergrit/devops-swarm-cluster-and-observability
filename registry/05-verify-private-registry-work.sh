187de34b085b18978665757b7e15f55fa6213816
Kergrit Robkop <kergrit@gmail.com>
2025-01-30 13:47:51 +0700

    Added registry stack deployment

---
+++ registry/05-verify-private-registry-work.sh
@@ -0,0 +1,8 @@
+# verify docker registry work
+docker login localhost:5000
+docker pull nginx:latest
+docker tag nginx:latest localhost:5000/nginx:latest
+docker push localhost:5000/nginx:latest
+
+docker stack deploy --with-registry-auth -c /home/ubuntu/efs/devops-swarm-cluster-and-observability/registry/nginx-stack.yml nginx
+docker stack ps nginx
