807cba796bea5567e24c7c8ea34e0cce3b08666f
Kergrit Robkop <kergrit@gmail.com>
2025-01-30 14:07:55 +0700

    Added ftp stack deployment

--- ftp/06-setup-ftp-server.sh
+++ ftp/06-setup-ftp-server.sh
@@ -8,3 +8,4 @@ docker push localhost:5000/ftp-server
 
 # deploy ftp-server stack
 docker stack deploy --with-registry-auth -c /home/ubuntu/efs/devops-swarm-cluster-and-observability/ftp/ftp-stack.yml ftp
+docker stack ps ftp
